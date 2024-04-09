eaa19e10685784d58c923bb478a85b87d50ff192
vitjmmf <160040331+vitjmmf@users.noreply.github.com>
2024-04-09 11:43:00 -0300

    Update NumeroDeLadosDoDado.java

--- NumeroDeLadosDoDado.java
+++ NumeroDeLadosDoDado.java
@@ -5,7 +5,7 @@ public class NumeroDeLadosDoDado {
     public static void main(String[] args) {
         String input = JOptionPane.showInputDialog(null, "Digite o número de lados do dado:");
         
-        // Convertendo o input para um número inteiro
+        // Convertendo o input para inteiro
         int numeroDeLados = Integer.parseInt(input);
         
         if (numeroDeLados <= 0) {
